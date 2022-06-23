b20d29d492093787763633d0b249df18c2825ae6
Kurlgargyey <nahsirkivar@gmail.com>
2022-06-23 17:01:17 +0200

    Add argument validation

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,9 @@
 class Game
 
-  def initialize
-    @board = 0
+  def initialize(cols = 7, rows = 6)
+    valid_cols = cols.to_i > 0 ? cols.to_i : 7
+    valid_rows = rows.to_i > 0 ? rows.to_i : 6
+    @board = Array.new(valid_cols) { Array.new(valid_rows) }
     @scores = Hash.new(0)
     @active_player = 1
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,6 +8,61 @@ describe Game do
     expect(board).not_to eq(nil)
   end
 
+  describe 'the board' do
+    subject(:game_board) { described_class.new }
+    
+    it 'is an array of size cols' do
+      board = game_board.instance_variable_get(:@board)
+      default_cols = 7
+      expect(board.length).to eq(default_cols)
+    end
+
+    it 'contains arrays of size rows' do
+      board = game_board.instance_variable_get(:@board)
+      default_rows = 6
+      board.each do |col|
+        expect(col.length).to eq(default_rows)
+      end
+    end
+
+    context 'with 10 cols and 4 rows' do
+      cols = 10
+      rows = 4
+      subject(:game_special_board) { described_class.new(cols, rows) }
+
+      it 'is an array of size 10' do
+        board = game_special_board.instance_variable_get(:@board)
+        expect(board.length).to eq(10)
+      end
+
+      it 'contains array of size 4' do
+        board = game_special_board.instance_variable_get(:@board)
+        board.each do |col|
+          expect(col.length).to eq(4)
+        end
+      end
+    end
+
+    context 'with bad arguments' do
+      bad_cols = 'afs'
+      bad_rows = nil
+      subject(:game_bad_board) { described_class.new(bad_cols, bad_rows) }
+
+      it 'uses default cols' do
+        board = game_bad_board.instance_variable_get(:@board)
+        expect(board.length).to eql(7)
+      end
+
+      it 'uses default rows' do
+        board = game_bad_board.instance_variable_get(:@board)
+        expect(board).not_to be_empty
+        board.each do |col|
+          expect(col.length).to eq(6)
+        end
+      end
+    end
+  end
+
   it 'keeps score in a hash' do
     scores = game_init.instance_variable_get(:@scores)
     expect(scores).to be_a(Hash)
@@ -22,7 +77,11 @@ describe Game do
     expect(game_init).to respond_to(:run_game)
   end
 
-  describe '#game_over?' do
+  describe '#run_game' do
+    subject(:game_start) { described_class.new }
 
+    it '' do
+      
+    end
   end
 end
