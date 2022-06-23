7179f27dbca3ca129a23548f8168df6ee1b262aa
Kurlgargyey <nahsirkivar@gmail.com>
2022-06-23 18:39:14 +0200

    Add #process_turn, #get_move and #make_move

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,13 @@
 class Game
+  attr_accessor :board
 
   def initialize(cols = 7, rows = 6)
-    valid_cols = cols.to_i > 0 ? cols.to_i : 7
-    valid_rows = rows.to_i > 0 ? rows.to_i : 6
-    @board = Array.new(valid_cols) { Array.new(valid_rows) }
+    @cols = cols.to_i > 0 ? cols.to_i : 7
+    @rows = rows.to_i > 0 ? rows.to_i : 6
+    @board = Array.new(@cols) { Array.new() }
     @scores = Hash.new(0)
-    @active_player = 1
+    @players = ['X', 'O']
+    @active_player = @players[0]
   end
 
   def run_game
@@ -14,5 +16,25 @@ class Game
 
   def game_over?
 
+  end
+
+  def process_turn
+    move = get_move
+    make_move(move)
+    @active_player = @players.rotate![0]
+  end
+
+  def get_move
+    move = -1
+    until move.between?(1, @cols)
+      move = gets.chomp.to_i
+    end
+    move
+  end
+
+  def make_move(col)
+    return board[col-1].push(@active_player) unless board[col-1].length == @rows
+    new_move = get_move
+    make_move(new_move)
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,19 +9,20 @@ describe Game do
   end
 
   describe 'the board' do
-    subject(:game_board) { described_class.new }
-    
-    it 'is an array of size cols' do
-      board = game_board.instance_variable_get(:@board)
-      default_cols = 7
-      expect(board.length).to eq(default_cols)
-    end
+      
+    context 'without arguments' do
+      subject(:game_board) { described_class.new }
+      it 'is an array of size cols' do
+        board = game_board.instance_variable_get(:@board)
+        default_cols = 7
+        expect(board.length).to eq(default_cols)
+      end
 
-    it 'contains arrays of size rows' do
-      board = game_board.instance_variable_get(:@board)
-      default_rows = 6
-      board.each do |col|
-        expect(col.length).to eq(default_rows)
+      it 'contains empty arrays' do
+        board = game_board.instance_variable_get(:@board)
+        board.each do |col|
+          expect(col.length).to eq(0)
+        end
       end
     end
 
@@ -35,10 +36,10 @@ describe Game do
         expect(board.length).to eq(10)
       end
 
-      it 'contains array of size 4' do
+      it 'contains empty arrays' do
         board = game_special_board.instance_variable_get(:@board)
         board.each do |col|
-          expect(col.length).to eq(4)
+          expect(col.length).to eq(0)
         end
       end
     end
@@ -53,11 +54,11 @@ describe Game do
         expect(board.length).to eql(7)
       end
 
-      it 'uses default rows' do
+      it 'contains empty arrays' do
         board = game_bad_board.instance_variable_get(:@board)
         expect(board).not_to be_empty
         board.each do |col|
-          expect(col.length).to eq(6)
+          expect(col.length).to eq(0)
         end
       end
     end
@@ -68,20 +69,148 @@ describe Game do
     expect(scores).to be_a(Hash)
   end
 
-  it 'starts with Player 1' do
+  it 'starts with X active' do
     active_player = game_init.instance_variable_get(:@active_player)
-    expect(active_player).to eq(1)
+    expect(active_player).to eq('X')
   end
 
   it 'responds to #run_game' do
     expect(game_init).to respond_to(:run_game)
   end
 
-  describe '#run_game' do
-    subject(:game_start) { described_class.new }
+  describe '#get_move' do
+    subject(:game_input) { described_class.new }
 
-    it '' do
-      
+    context 'when the input is a valid column number' do
+      before do
+        allow(game_input).to receive(:gets).and_return('3')
+      end
+    
+      it 'returns the input' do
+        expect(game_input.get_move).to eql(3)
+      end
+    end
+
+    context 'when there are out-of-bounds inputs' do
+      before do
+        allow(game_input).to receive(:gets).and_return('10', '11', 'g', '-1', '4', '5')
+      end
+
+      it 'calls #gets until it receives a valid input' do
+        expect(game_input).to receive(:gets).exactly(5).times
+        game_input.get_move
+      end
+
+      it 'returns the first valid input' do
+        expect(game_input.get_move).to eql(4)
+      end
     end
   end
+
+  describe '#make_move' do
+    subject(:game_move) { described_class.new }
+    selected_col = 4
+
+    context 'when X is active' do
+      
+      before do
+        game_move.instance_variable_set(:@active_player, 'X')
+      end
+
+      context 'when there are no other markers in the column' do
+        it 'places an X marker in the first place on the column' do
+          game_move.make_move(selected_col)
+          expect(game_move.board[selected_col-1].last).to eq('X')
+        end
+      end
+      
+      context 'when the column already has a marker' do
+        
+        before do
+          game_move.board[selected_col-1].push('O')
+        end
+
+        it 'keeps the previous marker' do  
+          game_move.make_move(selected_col)
+          expect(game_move.board[selected_col-1].last).to eq('X')
+          expect(game_move.board[selected_col-1][0]).to eq('O')
+        end
+      end
+
+      context 'when the column is already full' do
+        
+        before do
+          6.times{game_move.board[selected_col-1].push('O')}
+        end
+
+        new_move = 3
+
+        before do
+          allow(game_move).to receive(:get_move).and_return(new_move)
+        end
+
+        it 'asks for another move' do
+          expect(game_move).to receive(:get_move)
+          game_move.make_move(selected_col)
+        end
+
+        it 'makes the new move' do
+          game_move.make_move(selected_col)
+          expect(game_move.board[new_move-1].last).to eq('X')
+        end
+
+        it 'does not make the invalid move' do
+          expect(game_move.board[selected_col-1]).not_to receive(:push)
+          game_move.make_move(selected_col)
+        end
+
+        context 'when there is another invalid input' do
+          before do
+            allow(game_move).to receive(:get_move).and_return(selected_col, new_move)
+          end
+
+          it 'asks for another move again' do
+            expect(game_move).to receive(:get_move).twice
+            game_move.make_move(selected_col)
+          end
+
+          it 'makes the new move' do
+            game_move.make_move(selected_col)
+            expect(game_move.board[new_move-1].last).to eq('X')
+          end
+  
+          it 'does not make the invalid move' do
+            expect(game_move.board[selected_col-1]).not_to receive(:push)
+            game_move.make_move(selected_col)
+          end
+        end
+      end
+    end
+  end
+
+  describe '#process_turn' do
+    subject(:game_turn) { described_class.new }
+
+    before do
+      allow(game_turn).to receive(:get_move)
+      allow(game_turn).to receive(:make_move)
+    end
+
+    it 'asks for a move' do
+      expect(game_turn).to receive(:get_move)
+      game_turn.process_turn
+    end
+
+    it 'makes the move' do
+      expect(game_turn).to receive(:make_move)
+      game_turn.process_turn
+    end
+
+    it 'swaps the active player' do
+      previous_player = game_turn.instance_variable_get(:@active_player)
+      game_turn.process_turn
+      expect(game_turn.instance_variable_get(:@active_player)).not_to be(previous_player)
+    end
+  end
+
 end
